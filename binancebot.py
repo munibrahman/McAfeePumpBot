1b9c69db7ea8fbc81c0c38bfccf99aa5b81be658
returnzer0 <munibrhmn@gmail.com>
2017-12-24 23:34:17 -0700

    Finished bot, added block for not buying retweets, added variable for amount that you want to gamble with (in BTC).

--- binancebot.py
+++ binancebot.py
@@ -1,5 +1,6 @@
 from binance.client import Client
 import Settings
+import json
 
 api_key = Settings.BINANCE_API_KEY
 api_secret = Settings.BINANCE_API_SECRET
@@ -7,13 +8,49 @@ api_secret = Settings.BINANCE_API_SECRET
 
 client = Client(api_key, api_secret)
 
+# I didn't know this endpoint existed, will use this endpoint next time.
+# print(client.get_products())
+
 # get all symbol prices
 prices = client.get_all_tickers()
 
 listBtc = []
-# TODO: Get all the symbols from Binance, if all holdings in BTC then you only want to trade with BTC, otherwise filter
-# eth values or bnb values
+# Filtering out results that don't contain BTC
 for eachPrice in prices:
     if 'btc' in eachPrice['symbol'].lower():
-        listBtc.append(eachPrice)
+        listBtc.append(eachPrice['symbol'])
         print(eachPrice['symbol'])
+
+print(listBtc)
+
+# This list will contain all the tickers without the tracker suffix of BTC
+list_without_btc = []
+
+for eachTicker in listBtc:
+    eachNewTicker = eachTicker[:-3]
+    list_without_btc.append(eachNewTicker)
+
+print(list_without_btc)
+
+# This file contains a list of almost all known currencies, pulled from github
+allCurrencies = json.loads(open('cryptocurrencies.json').read())
+
+# Data object will hold a key value pair of each tracker
+# Key will be the shorthand ticker, value will hold the full name
+# { BTC : 'Bitcoin'}
+data = {}
+
+for eachTicker in list_without_btc:
+    for eachCurrency in allCurrencies:
+        if eachCurrency == eachTicker:
+            data[eachTicker] = allCurrencies[eachCurrency]
+            continue
+
+
+json_data = json.dumps(data)
+print(json_data)
+
+# Dump all this data into a file, so that it can be used by the twitter main.py script
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
+
--- main.py
+++ main.py
@@ -6,6 +6,19 @@ from tweepy import Stream
 import Settings
 
 import json
+import tweepy
+import re
+
+from binance.client import Client
+
+import binancebot
+
+api_key = Settings.BINANCE_API_KEY
+api_secret = Settings.BINANCE_API_SECRET
+
+
+client = Client(api_key, api_secret)
+
 
 settings = Settings
 
@@ -19,6 +32,58 @@ consumer_secret = settings.CONSUMER_SECRET
 access_token = settings.ACCESS_TOKEN
 access_token_secret = settings.ACCESS_TOKEN_SECRET
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+currencies = json.loads(open('data.json').read())
+
+binancebot
+
+BTC_to_gamble = 0.5
+
+
+def buy_currency(currency):
+    ticker = currency + 'BTC'
+    quantity = int(BTC_to_gamble / float(client.get_orderbook_ticker(symbol=ticker)['askPrice']))
+    order = client.create_order(
+        symbol=ticker,
+        side=Client.SIDE_BUY,
+        type=Client.ORDER_TYPE_MARKET,
+        quantity=quantity)
+
+    print(order)
+
+
+# Take the input tweet and split it into a list of strings.
+# Compare those strings against the database and buy the coins needed.
+def decipher(json_tweet):
+    print(json_tweet)
+    if "created_at" in json_tweet:
+        # This ensures that the tweet is ONLY by McAfee and not someone else tweeting at him
+        if json_tweet["user"]["id"] == int(Settings.MCAFEE_TWITTER_ID):
+            # We also want to make sure that he isn't favouring/retweeting someone else's tweet
+            if not "retweeted_status" in json_tweet:
+                # Get the full, extended tweet with all of the words.
+                extended_tweet = api.get_status(json_tweet["id"], tweet_mode='extended')._json['full_text']
+                print(extended_tweet)
+                # Removes any sort of symbols using regex
+                removed_punctuation = re.sub(r'[^\w]', ' ', extended_tweet)
+                # Splits all of the words into a list of strings
+                listOfWords = removed_punctuation.split()
+                print(listOfWords)
+
+                #  Compare each word in the tweet against the listed currencies on binance, if we find anything, BUY
+                for eachWord in listOfWords:
+                    for eachCurrency in currencies:
+                        if eachWord.lower() == eachCurrency.lower() or eachWord.lower() == currencies[eachCurrency].lower():
+                            print(eachCurrency)
+                            print('BUY')
+                            buy_currency(eachCurrency)
+                            continue
+
+
 class StdOutListener(StreamListener):
     """ A listener handles tweets that are received from the stream.
     This is a basic listener that just prints received tweets to stdout.
@@ -27,8 +92,9 @@ class StdOutListener(StreamListener):
     # TODO: Right now ALL tweets mentioning mcafee are being broadcasted, make sure you only pass new tweets from
     # mcafee himself
     def on_data(self, data):
-        j = json.loads(data)
-        print(j["text"])
+        print('\n' + data + '\n')
+        json_tweet = json.loads(data)
+        decipher(json_tweet)
         return True
 
     def on_error(self, status):
@@ -42,7 +108,6 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
     stream.filter(follow=[settings.MCAFEE_TWITTER_ID])
 
-# TODO: Take the input tweet and split it into a list of strings.
-# Compare those strings against the database and buy the coins needed.
-# def decipher(tweet):
-#     print(tweet)
+
+
+
